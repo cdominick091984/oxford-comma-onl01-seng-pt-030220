2f2a2bd05f3f251a64710fab622986eb0c76941c
Domenico Cascone <dcascone84@gmail.com>
2020-03-10 18:11:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
   elsif array.length == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    "#{array[1].insert(0, "and")}"
+    "#{array[-1].insert(0, "and ")}"
   end
   binding.pry
 end
