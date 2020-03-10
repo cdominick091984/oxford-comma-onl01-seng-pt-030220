6a8f6a767e620944150b5df859c5212bbe06d456
Domenico Cascone <dcascone84@gmail.com>
2020-03-10 17:52:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
   elsif array.length == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    "#{array.insert(-2, "and")
-       array.join(", ")}"
-
+    #["dog", "cat", "mouse"]
+    "#{array.insert(-2, "and")}"
   end
+  binding.pry
 end
